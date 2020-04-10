da9bb420c7eed10dbbd4bcd2c43dd3e71fa48813
Marcos Moreno Solana <marcos.moreno.solana@alumnos.upm.es>
2020-04-10 21:24:44 +0200

    Signed-off-by: Marcos Moreno Solana <marcos.moreno.solana@alumnos.upm.es>

---
+++ sources/version.el
@@ -0,0 +1,7 @@
+-- Define the version, date, author and comments associated to this library
+#LIBRARY_INFO
+	#VERSION = "0.1"
+	#DATE = "03/04/2020"
+	#AUTHOR = "Marcos"
+	#COMMENTS= "Ejemplos de las guias"
+END #LIBRARY_INFO
